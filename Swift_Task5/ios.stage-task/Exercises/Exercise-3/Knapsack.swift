d3d6f89f3d482d53fe954dbd4c4c3a42decc99e4
AnthonyDavydchyk <62406462+AnthonyDavydchyk@users.noreply.github.com>
2021-06-14 22:30:56 +0300

    feat: add solution for Ex3, Knapsack task

--- Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
@@ -17,6 +17,57 @@ public final class Knapsack {
     }
     
     func findMaxKilometres() -> Int {
-        return -1
+        var distanceFoods = [[Int]]()
+        distanceFoods.reserveCapacity(foods.count)
+        for i in 0..<foods.count {
+            var food = [Int]()
+            food.reserveCapacity(maxWeight)
+            for j in 1...maxWeight {
+                if i == 0 && foods[i].weight <= j {
+                    food.append(foods[i].value)
+                    continue
+                } else if i == 0 {
+                    food.append(0)
+                    continue
+                }
+                if foods[i].weight <= j {
+                    food.append(max(distanceFoods[i-1][j-1], (foods[i].value + ((j - foods[i].weight-1 >= 0) ? distanceFoods[i-1][j-foods[i].weight-1] : 0))))
+                } else {
+                    food.append(distanceFoods[i-1][j-1])
+                }
+            }
+            distanceFoods.append(food)
+        }
+        var distanceDrinks = [[Int]]()
+        distanceDrinks.reserveCapacity(drinks.count)
+        for i in 0..<drinks.count {
+            var drink = [Int]()
+            drink.reserveCapacity(maxWeight)
+            for j in 1...maxWeight {
+                if i == 0 && drinks[i].weight <= j {
+                    drink.append(drinks[i].value)
+                    continue
+                } else if i == 0 {
+                    drink.append(0)
+                    continue
+                }
+                if drinks[i].weight <= j {
+                    drink.append(max(distanceDrinks[i-1][j-1], (drinks[i].value + ((j - drinks[i].weight-1 >= 0) ? distanceDrinks[i-1][j-drinks[i].weight-1] : 0))))
+                } else {
+                    drink.append(distanceDrinks[i-1][j-1])
+                }
+            }
+            distanceDrinks.append(drink)
+        }
+        var result = Int()
+        for i in 1..<maxWeight {
+            let foodSet = distanceFoods[foods.count-1][maxWeight-i-1], drinkSet = distanceDrinks[drinks.count-1][i-1]
+            let tmpRes = (foodSet <= drinkSet ? foodSet : drinkSet)
+            if foodSet != 0 && drinkSet != 0 && tmpRes >= result{
+                result = tmpRes
+            }
+        }
+
+        return result
     }
 }
