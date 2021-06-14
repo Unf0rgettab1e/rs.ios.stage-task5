6bd4ac3eb211a2a897e3bccb05073810b0faafc0
AnthonyDavydchyk <62406462+AnthonyDavydchyk@users.noreply.github.com>
2021-06-14 14:52:08 +0300

    feat: add solution for Ex1

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,49 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        var maxValues = [Int]()
+        var maxProfit = 0, begin = 0
+        if prices.count > 1 {
+            for i in 1..<prices.count {
+                if (i+1 < prices.count && prices[i] >= prices[i+1] && prices[i] > prices[i-1]) || (prices[i] > prices[i-1] && i+1 == prices.count) {
+                    maxValues.append(i)
+                }
+            }
+            let countMax = maxValues.count
+            if countMax > 1 {
+                maxValues = deleteUnnecessaryItem(maxValues, prices, 0)
+            }
+            
+            for it in maxValues{
+                for i in begin..<it{
+                    if prices[it] - prices[i] > 0 {
+                        maxProfit += (prices[it] - prices[i])
+                    }
+                }
+                begin = it + 1
+            }
+        }
+        return maxProfit
     }
+
+    func deleteUnnecessaryItem(_ maxValues: [Int], _ prices: [Int], _ begin: Int) -> [Int] {
+        var tempMax = 0, copyMaxValues = maxValues, at = 0, countMax = maxValues.count
+        if begin < countMax {
+            for j in begin..<countMax {
+                if prices[copyMaxValues[j]] > tempMax {
+                    tempMax = prices[copyMaxValues[j]]
+                    at = j
+                }
+            }
+            let beginNew = begin + 1
+            if at != 0 {
+                copyMaxValues.removeSubrange(begin..<at)
+            }
+            if at != countMax {
+                copyMaxValues = deleteUnnecessaryItem(copyMaxValues, prices, beginNew)
+            }
+        }
+        return copyMaxValues
+    }
+
 }
