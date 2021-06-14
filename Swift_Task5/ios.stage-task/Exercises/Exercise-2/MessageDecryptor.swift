ee0afd778f2c1f1b7f906171ba534fb9f7e238e9
AnthonyDavydchyk <62406462+AnthonyDavydchyk@users.noreply.github.com>
2021-06-14 23:55:14 +0300

    feat: add solution for Ex2

--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,34 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+        var number = 1, numberStr = "", result = message, counter = 0, begin = 0, end = 0, copyMessage = message
+
+        for char in message {
+            if char.isNumber {
+                numberStr.append(char)
+            }
+            var check = true, checkCounter = false
+            if char == "[" {
+                counter += 1
+                number = Int(number)
+                check = false
+                checkCounter = true
+            }
+            if char == "]" {
+                counter -= 1
+            }
+            if counter == 0 && checkCounter {
+                let start = message.index(message.startIndex, offsetBy: begin)
+                let endi = message.index(message.startIndex, offsetBy: end)
+                result += decryptMessage(String(message[start..<endi]))
+            }
+            if check {
+                begin += 1
+            }
+            end += 1
+            
+        }
+        result = String(repeating: result, count: number)
+        return result
     }
 }
