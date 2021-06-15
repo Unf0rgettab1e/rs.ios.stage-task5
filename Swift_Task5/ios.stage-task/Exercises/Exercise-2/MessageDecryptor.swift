71f1b20a81b2fdb2e846176909e661e9e8fa0d3b
AnthonyDavydchyk <62406462+AnthonyDavydchyk@users.noreply.github.com>
2021-06-15 03:37:39 +0300

    fix: upgrade MessageDecryptor in Ex2 to pass all the tests

--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,34 +3,47 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        var number = 1, numberStr = "", result = message, counter = 0, begin = 0, end = 0, copyMessage = message
-
+        var number = 1, numberStr = "", result = "", counter = 0, begin = 0, end = 0
+        var check = true, checkCounter = false, checkNumber = true
         for char in message {
-            if char.isNumber {
+            if char.isNumber && checkNumber {
                 numberStr.append(char)
             }
-            var check = true, checkCounter = false
-            if char == "[" {
+            else if char == "[" {
                 counter += 1
-                number = Int(number)
+                number = Int(numberStr) ?? 0
                 check = false
                 checkCounter = true
+                checkNumber = false
             }
-            if char == "]" {
+            else if char == "]" {
                 counter -= 1
+                checkNumber = true
+            }
+            else if check {
+                result.append(char)
             }
             if counter == 0 && checkCounter {
-                let start = message.index(message.startIndex, offsetBy: begin)
+                let start = message.index(message.startIndex, offsetBy: begin+1)
                 let endi = message.index(message.startIndex, offsetBy: end)
-                result += decryptMessage(String(message[start..<endi]))
+                let tmpResult = decryptMessage(String(message[start..<endi]))
+                if numberStr != "" {
+                    result += String(repeating: tmpResult, count: number)
+                    numberStr = ""
+                } else {
+                    result += tmpResult
+                }
+                begin = end
+                checkCounter = false
+                check = true
+                print(result)
             }
             if check {
                 begin += 1
             }
             end += 1
-            
         }
-        result = String(repeating: result, count: number)
+        
         return result
     }
 }
